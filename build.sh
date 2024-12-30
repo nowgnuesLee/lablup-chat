f412ee494660844b70e7f3ed9967f581fa595d8f
nowgnuesLee <seungwon@lablup.com>
2024-12-30 14:32:14 +0900

    ci: add backend.Dockerfie & update docekr-compose

--- build.sh
+++ build.sh
@@ -1,23 +1,9 @@
 #!/bin/bash
 
 # 변수 설정
-IMAGE_NAME="chat-front:latest"
-DOCKERFILE="frontend.Dockerfile"
 DOCKER_COMPOSE_FILE="docker-compose.yml"
 BUILD_CONTEXT="."
 PORT=3001
-
-# 이미지 빌드
-echo "Building Docker image: $IMAGE_NAME"
-docker build -t $IMAGE_NAME -f $DOCKERFILE $BUILD_CONTEXT
-
-# 빌드 성공 여부 확인
-if [ $? -eq 0 ]; then
-  echo "Docker image $IMAGE_NAME built successfully!"
-else
-  echo "Failed to build Docker image $IMAGE_NAME."
-  exit 1
-fi
 
 # 도커 컴포즈 기준으로 컨테이너 내리기
 echo "Stopping and removing existing Docker container..."
@@ -25,7 +11,7 @@ docker compose -f $DOCKER_COMPOSE_FILE down || true
 
 # 도커 컴포즈 기준으로 컨테이너 올리기
 echo "Starting Docker container..."
-docker compose -f $DOCKER_COMPOSE_FILE up -d
+docker compose -f $DOCKER_COMPOSE_FILE up --build -d
 
 # 컨테이너 실행 여부 확인
 if [ $? -eq 0 ]; then
